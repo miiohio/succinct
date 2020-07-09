921b09afba206e8125f1187e48333047e3205e82
William Harvey <drwjharvey@gmail.com>
2020-07-09 11:39:43 -0400

    Finagle README for PyPI consumption

--- setup.py
+++ setup.py
@@ -4,14 +4,23 @@ from setuptools import setup, find_namespace_packages
 with open("succinct/VERSION") as version_file:
     version = version_file.read().strip()
 
+kwds = {}
+try:
+    kwds['long_description'] = open('README.md').read()
+    kwds['long_description_content_type'] = 'text/markdown'
+except IOError:
+    pass
+
 setup(
     name="succinct",
     version=version,
     author="William Harvey",
+    author_email="drwjharvey@gmail.com",
     description="Succinct, compact, and compressed data structures for data-intensive applications",
     url="https://github.com/miiohio/succinct",
     python_requires=">=3.7.6",
     packages=["succinct"] + find_namespace_packages(include=["succinct.*"]),
     package_data={"succinct": ["VERSION", "py.typed"]},
     install_requires=[],
+    **kwds
 )
